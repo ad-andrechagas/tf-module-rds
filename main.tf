b7e207d595e64c8e377b2d39b80ad75e313b12a1
Andre Chagas <143511353+ad-andrechagas@users.noreply.github.com>
2023-09-20 17:50:57 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "aws_db_instance" "rds-lab" {
-  allocated_storage           = 20
-  storage_type                = "gp2"
-  engine                      = "mysql"
-  engine_version              = "5.7"
-  instance_class              = var.db_instance_class
-  db_name                     = "var.db_name"
-  username                    = "labadmin"
-  password                    = "labadmin"
-  parameter_group_name        = "default.mysql5.7"
-  manage_master_user_password = "true"
+  identifier = "var.db_identifier"
+
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = var.db_instance_class
+  username             = "labadmin"
+  password             = "labadmin123"
+  parameter_group_name = "default.mysql5.7"
+  #manage_master_user_password = "true"
   # Add more RDS configuration options as needed
 }
-
