15a6ca9dac7742a7b9cf25b95765c380bf2fa0bd
Chagas, Andre <alm2.servicos.ti@gmail.com>
2023-09-21 15:10:20 -0300

    Minor change in main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ resource "aws_db_instance" "rds-lab" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = var.db_instance_class
+  instance_class       = "${var.db_instance_class}"
   username             = "labadmin"
   password             = "labadmin123"
   parameter_group_name = "default.mysql5.7"
